52f938e6a5686342537387c15549cf59e39267e2
dorstandsgn-hash <dorstandsgn@gmail.com>
2025-12-16 11:44:43 +0200

    Add weapom class

--- RPG_game/RPG_game/Enemy.cs
+++ RPG_game/RPG_game/Enemy.cs
@@ -5,6 +5,31 @@ using static RPG_game.Enemy;
 
 namespace RPG_game
 {
+    public class Weapon : Item, IEquippable
+    {
+        public int Damage { get; protected set; }
+        public Weapon(string name, string description, int damage) : base(name, description)
+        {
+            Damage = damage;
+        }
+
+        public void Equip(Player player)
+        {
+            Console.WriteLine($"{Name} екіпійовано! (+{Damage} До атаки)");
+        }
+
+        public void Unequip(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Use(Player player)
+        {
+            Console.WriteLine($"{ player Name} екіпірує {Name}");
+            Equip(player);
+        }
+    }
+
     public class Player : Character, ISpellCaster
     {
         private int _mana;
